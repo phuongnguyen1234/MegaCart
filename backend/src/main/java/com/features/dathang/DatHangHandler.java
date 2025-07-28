a6ae785742846edbc50d558e4638ba7cbf256480
phuongnguyen1234 <nguyenhoangducphuong1@gmail.com>
2025-07-28 21:03:54 +0700

    thêm các file mẫu

---
+++ backend/src/main/java/com/features/dathang/DatHangHandler.java
@@ -0,0 +1,13 @@
+package com.features.dathang;
+
+public class DatHangHandler {
+    static class DatHangController {}
+
+    static class DatHangService {}
+
+    static class DatHangRepository {}
+
+    static class DonHangDTO1{} //DTO in
+
+    static class DonHangDTO2{} //DTO out
+}
---
+++ backend/src/main/java/com/features/dathang/laythongtintaikhoan/LayThongTinTaiKhoanHandler.java
@@ -0,0 +1,9 @@
+package com.features.dathang.laythongtintaikhoan;
+
+public class LayThongTinTaiKhoanHandler {
+    static class LayThongTinTaiKhoanController{}
+
+    static class LayThongTinTaiKhoanService{}
+
+    static class LayThongTinTaiKhoanRepository{}
+}
