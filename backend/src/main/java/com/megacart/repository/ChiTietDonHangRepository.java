1d47e58aeae1277da180ed64f6f70b4afb1a6ea7
Kien04840 <kien231204@gmail.com>
2025-08-17 20:54:02 +0700

    Fix nhỏ

--- backend/src/main/java/com/megacart/repository/ChiTietDonHangRepository.java
+++ backend/src/main/java/com/megacart/repository/ChiTietDonHangRepository.java
@@ -1,9 +1,10 @@
 package com.megacart.repository;
 
 import com.megacart.model.ChiTietDonHang;
+import com.megacart.model.ChiTietDonHangId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChiTietDonHangRepository extends JpaRepository<ChiTietDonHang, Integer> {
+public interface ChiTietDonHangRepository extends JpaRepository<ChiTietDonHang, ChiTietDonHangId> {
 }
